55ee2e5f94f331935bf809393503902f8a2b1ceb
Aissa <sadoudi17@gmail.com>
2024-02-01 17:57:27 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import TicTacToe from "./Components/TicTacToe/TicTacToe.js"
+import './App.css'
+
+export default function App(){
+
+
+return (
+<div>
+<TicTacToe/>
+</div>
+)
+}
